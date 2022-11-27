55e2e543c6bd03cb4af60633ddc8e636f24e43e0
est0y <asteoon@gmail.com>
2022-11-27 20:22:07 +0300

    homework07

---
+++ homework07/src/main/java/ru/est0y/game/app/Player.java
@@ -0,0 +1,13 @@
+package ru.est0y.game.app;
+
+public class Player {
+    private final String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ homework07/src/main/java/ru/est0y/game/tests/fakes/FakeDice.java
@@ -0,0 +1,22 @@
+package ru.est0y.game.tests.fakes;
+
+import ru.est0y.game.app.Dice;
+
+public class FakeDice {
+    public static Dice getDiceWithDefiniteResults(int... results) {
+        return new Dice() {
+
+            private int callCount = 0;
+
+            @Override
+            public int roll() {
+                callCount++;
+                try {
+                    return results[callCount - 1];
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    throw new IllegalArgumentException("Передано неверное колличество результатов", e);
+                }
+            }
+        };
+    }
+}
